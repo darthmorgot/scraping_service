f4506013a9338bbd65580a6e1bb6ad79f9aeb984
darthmorgot <dartmorgot@yandex.ru>
2022-07-20 16:05:36 +1000

    Изменил код файла, чтобы при его запуске данные добавлялись в базу данных.

--- run_scraping.py
+++ run_scraping.py
@@ -1,6 +1,18 @@
 import codecs
+import os
+import sys
+
+from django.db import DatabaseError
+
+project_path = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(project_path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'scraping_service.settings'
+
+import django
+django.setup()
 
 from scraping.parsers import parse_superjob, parse_headhunter, parse_rabota
+from scraping.models import Vacancy, City, Language, Error
 
 parsers = (
     (parse_superjob, 'https://habarovsk.superjob.ru/vacancy/search/?keywords=python'),
@@ -8,6 +20,9 @@ parsers = (
      'https://khabarovsk.hh.ru/search/vacancy?text=python&from=suggest_post&fromSearchLine=true&area=102'),
     (parse_rabota, 'https://khabarovsk.rabota.ru/?query=python&sort=relevance')
 )
+
+city = City.objects.filter(slug='habarovsk').first()
+language = Language.objects.filter(slug='python').first()
 
 data = []
 errors = []
@@ -17,5 +32,15 @@ for func, url in parsers:
     data += data_result
     errors += errors_result
 
-with codecs.open('parsing_result.txt', 'w', encoding='utf-8') as file_handler:
-    file_handler.write(str(data))
+for item in data:
+    vacancy = Vacancy(**item, city=city, language=language)
+    try:
+        vacancy.save()
+    except DatabaseError:
+        pass
+
+if errors:
+    ers = Error(data=errors).save()
+
+# with codecs.open('parsing_result.txt', 'w', encoding='utf-8') as file_handler:
+#     file_handler.write(str(data))
