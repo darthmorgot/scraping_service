64930bbd0b9156eb47c4571b8b7fd87eee109da0
darthmorgot <dartmorgot@yandex.ru>
2022-07-24 19:39:22 +1000

    Создал файл для обработки форм входа и регистрации.

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+
+class UserLoginForm(forms.Form):
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean(self, *args, **kwargs):
+        email = self.cleaned_data.get('email').strip()
+        password = self.cleaned_data.get('password').strip()
+
+        if email and password:
+            user_data = User.objects.filter(email=email)
+            if not user_data.exists():
+                raise forms.ValidationError('Такого пользователя нет')
+            if not check_password(password, user_data[0].password):
+                raise forms.ValidationError('Неверный пароль')
+            user = authenticate(email=email, password=password)
+            if not user:
+                raise forms.ValidationError('Данный пользователь не активен')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
