94c7cec1a44a115aa97282a3a1797e4f60917bd9
darthmorgot <dartmorgot@yandex.ru>
2022-07-26 16:26:01 +1000

    Добавил функции-контроллеры для отображения страницы с формой обновления данных пользователя.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
-from accounts.forms import UserLoginForm, UserRegistrationForm
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+
+User = get_user_model()
 
 
 def login_view(request):
@@ -30,3 +32,31 @@ def register_view(request):
         new_user.save()
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST or None)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                return redirect('update')
+
+        form = UserUpdateForm(initial={'city': user.city, 'language': user.language, 'send_email': user.send_email})
+        return render(request, 'accounts/update.html', {'form': form})
+    else:
+        return redirect('login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            user_data = User.objects.get(pk=user.pk)
+            user_data.delete()
+    return redirect('home')
