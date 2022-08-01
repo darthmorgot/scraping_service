75a59160d598def20f3b18412b111457fcf1acaa
darthmorgot <dartmorgot@yandex.ru>
2022-08-01 18:13:01 +1000

    Добавил путь к странице удаления вакансии, и переменную с названием приложения.

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from scraping.views import home_view, list_view, detail_view, DetailPageView, ListPageView, CreatePageView, \
-    UpdatePageView
+    UpdatePageView, DeletePageView
+
+app_name = 'scraping'
 
 urlpatterns = [
     # path('detail/<int:pk>/', detail_view, name='detail'),
@@ -9,6 +11,7 @@ urlpatterns = [
     path('detail/<int:pk>/', DetailPageView.as_view(), name='detail'),
     path('create/', CreatePageView.as_view(), name='create'),
     path('update/<int:pk>/', UpdatePageView.as_view(), name='update'),
+    path('delete/<int:pk>/', DeletePageView.as_view(), name='delete'),
     path('list/', ListPageView.as_view(), name='list'),
     path('', home_view, name='home'),
 ]
