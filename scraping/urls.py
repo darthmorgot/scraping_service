8151e2fd6fe949eca13e24fdfae8eef449927c58
darthmorgot <dartmorgot@yandex.ru>
2022-08-01 13:53:05 +1000

    Добавил путь к странице показа одной вакансии.

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from scraping.views import home_view, list_view
+from scraping.views import home_view, list_view, detail_view, DetailPageView
 
 
 urlpatterns = [
+    # path('detail/<int:pk>/', detail_view, name='detail'),
+    path('detail/<int:pk>/', DetailPageView.as_view(), name='detail'),
     path('list/', list_view, name='list'),
     path('', home_view, name='home'),
 ]
