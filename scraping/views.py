ef22041609e3abb714141d278c026d8c4c66f266
darthmorgot <dartmorgot@yandex.ru>
2022-08-01 14:55:19 +1000

    Добавил класс-контроллер для отображения страницы списка вакансий.

--- scraping/views.py
+++ scraping/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from scraping.forms import SearchForm
 from scraping.models import Vacancy
@@ -49,3 +49,32 @@ def detail_view(request, pk):
 class DetailPageView(DetailView):
     queryset = Vacancy.objects.all()
     template_name = 'scraping/detail.html'
+
+
+class ListPageView(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+    form = SearchForm()
+    context_object_name = 'vacancies'
+    paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = self.form
+        context['vacancies'] = context['page_obj']
+        return context
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        page_vacancies = []
+        if city or language:
+            filtered = {}
+            if city:
+                filtered['city__slug'] = city
+            if language:
+                filtered['language__slug'] = language
+            page_vacancies = Vacancy.objects.filter(**filtered)
+        return page_vacancies
