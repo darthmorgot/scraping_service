a7e7b0abc69366d206475fdc8ebd141ce5eee620
darthmorgot <dartmorgot@yandex.ru>
2022-08-01 15:23:16 +1000

    Добавил путь к странице создания вакансии.

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from scraping.views import home_view, list_view, detail_view, DetailPageView, ListPageView
+from scraping.views import home_view, list_view, detail_view, DetailPageView, ListPageView, CreatePageView
 
 
 urlpatterns = [
     # path('detail/<int:pk>/', detail_view, name='detail'),
     # path('list/', list_view, name='list'),
     path('detail/<int:pk>/', DetailPageView.as_view(), name='detail'),
+    path('create/', CreatePageView.as_view(), name='create'),
     path('list/', ListPageView.as_view(), name='list'),
     path('', home_view, name='home'),
 ]
