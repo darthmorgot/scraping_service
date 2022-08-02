2d168f626ae7f0124f87a5d7fa98eda3d7d798e9
darthmorgot <dartmorgot@yandex.ru>
2022-08-02 18:53:18 +1000

    Добавил адрес проекта для размещения на хостинге.

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -38,7 +38,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['scrap-training.herokuapp.com']
 
 
 # Application definition
